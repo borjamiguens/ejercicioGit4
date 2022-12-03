e54252999f86b8ed4901117b3d58cf1ea22b9b85
borja <borja@example.com>
2022-12-03 18:27:01 +0100

    diseñamos la prueba que muestra el error

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,11 @@ public class Main {
         Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
 
+        p.edad = -31; // Ahora acepta edades negativas
+
+        Empleado p2 = new Empleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
+        System.out.println(p);
+
+        
     }
 }
