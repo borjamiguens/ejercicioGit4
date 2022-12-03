daa533007e8b3b4bf4b1f2cb3ecb8d20da971f45
borja <borja@example.com>
2022-12-03 18:31:06 +0100

    information hidding:edad es solo modificable a través de un setter

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,17 +1,22 @@
 import java.util.Date;
 
 class Empleado {
-    final String nombre;
-    Date birthDate;
+    String nombre;
+    private int edad;
+    Date fechaContratacion;
 
-    Empleado(String name, Date birthDate) {
-        this.nombre = name;
-        this.birthDate = birthDate;
+    public Empleado(String nombre, int edad, Date fechaContratacion) {
+        @@ -11,6 +11,11 @@ public Empleado(String nombre, int edad, Date fechaContratacion) {
+            this.fechaContratacion = fechaContratacion;
+        }
+
+        public void setEdad(int edad) {
+            if (edad<0) throw new RuntimeException("Un empleado no puede tener edad negativa");
+            else this.edad = edad;
+        }
+
+        @Override
+        public String toString() {
+            return "Se ha registrado a " + nombre + " (" + edad + " años; contratado en 19" + fechaContratacion.getYear() + ")";
+        }
     }
-
-    @Override
-    public String toString() {
-        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
-    }
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
 import java.util.Calendar;
 import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
 
-
-        Persona persona = new Persona();
-        persona.nombre = "Pepe Pérez";
-        persona.edad = 31;
-        persona.fechaContratacion = new Date(90, Calendar.FEBRUARY, 1);
-
-        System.out.println("Se ha registrado a " + persona.nombre + " (" + persona.edad + " años; contratado en 19" + persona.fechaContratacion.getYear() + ")");
-
-        Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
+        Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
 
-        p.edad = -31; // Ahora acepta edades negativas
+        //p.edad = -31; // Arreglado: error de compilación. edad no es accesible
 
         Empleado p2 = new Empleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
-
-        
     }
 }
